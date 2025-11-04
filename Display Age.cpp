89ea56c657266578bff63b4fc07c3eca32ccefda
celdrick95 <celdrickmadla09@gmail.com>
2025-11-04 16:20:03 +0800

    Add files via upload

---
+++ Display Age.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // PERSONAL INFORMATION
+    cout << "NAME:CELDRICK NHELD SAMPILO MADLA \n";
+    int myAge = 19;
+    cout << "AGE:" << myAge << "\n";
+    cout << "GENDER:MALE \n";
+    cout << "ADDRESS:BAGONG SILANG CALOOCAN CITY \n";
+    cout << "CONTACT NUMBER:0999999999 \n";
+    cout << "PROGRAM:BACHELOR OF SCIENCE IN INFROMATION TECHNOLOGY \n";
+    return 0;
+}
